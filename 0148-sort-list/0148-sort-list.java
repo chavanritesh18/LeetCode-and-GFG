fe6a6ca8206709d3ec4a07b8d923f544635687c8
Chavan Ritesh <140919594+chavanritesh18@users.noreply.github.com>
2024-03-15 17:55:27 +0530

    0148-sort-list.java

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -14,20 +14,25 @@ class Solution {
         ListNode temp =  head;
         
         ArrayList<Integer> arr  = new ArrayList<>();
-        
+
+        // Traverse the linked list and add all the elements of the linked list in the array list
         while(temp  != null)
         {
             arr.add(temp.val);
             temp = temp.next;
         }
-        
+
+        // after adding the elements in the linked list sort them by using collections sort method in java
         
         Collections.sort(arr);
         
-        int i = 0;
-        
+        int i = 0; // for iterating over the arraylist
+
+
+        // assing the temp again to head
         temp  = head;
-        
+
+        // traverse again and assign the sorted values to the linked list
         while(temp != null)
         {
             temp.val =  arr.get(i);
@@ -35,7 +40,9 @@ class Solution {
             temp  = temp.next;
             
         }
-        
+
+
+        // return the head of the linked list as all elments are stored in sorted order.
         return head;
     }
 }
